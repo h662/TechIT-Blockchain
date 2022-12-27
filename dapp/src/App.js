0592db45b6c095fc577165b08ac183c98c93fcc4
h662Olbm <h662@olbm.app>
2022-12-27 18:47:55 +0900

    3. Chakra UI Setting

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div>Hello, TechIT!</div>;
-}
+import { Box, Button } from "@chakra-ui/react";
+
+const App = () => {
+  return <Box>Hello, TechIT!</Box>;
+};
 
 export default App;
